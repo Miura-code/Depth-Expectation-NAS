69eda9c3933025379ebb47bbb760c615e174f93b
miura-ice9 <smiuymiu0official0@gmail.com>
2024-07-12 15:19:57 +0900

    doc: 教師ネット学習用のシェルスクリプトを更新

--- run_finetune.sh
+++ run_finetune.sh
@@ -1,66 +1,48 @@
 #!/bin/bash
 
+type=$1
+if [ ${train} = "train" ]; then
 # ===== モデルをファインチューニング =====
-# name=$1
-
-# teacher_model=$2
-# save=$3
-# dataset=cifar100
-# cutout=16
-# batch_size=64
-# epoch=300
-# seed=0
-# train_portion=0.9
-# python finetuneTeacher_main.py \
-#     --name $name \
-#     --model_name $teacher_model\
-#     --dataset $dataset\
-#     --cutout_length $cutout\
-#     --batch_size $batch_size \
-#     --epochs $epoch \
-#     --seed $seed \
-#     --save $save \
-#     --train_portion $train_portion \
-#     --w_lr 0.1 \
-#     --w_grad_clip 100.
-
-# ===== モデルをファインチューニング =====
-# name=$1
-
-# teacher_model=$2
-# save=$3
-# dataset=cifar100
-# cutout=16
-# batch_size=64
-# epoch=300
-# seed=0
-# train_portion=0.9
-# python trainTeacher_main.py \
-#     --name $name \
-#     --model_name $teacher_model\
-#     --dataset $dataset\
-#     --cutout_length $cutout\
-#     --batch_size $batch_size \
-#     --epochs $epoch \
-#     --seed $seed \
-#     --save $save \
-#     --train_portion $train_portion \
-#     --lr 0.1 \
-
-## ===== 複数エポック数でファインチューニングする =====
-# for epoch in 100 200 300; do
-#     python finetuneTeacher_main.py \
-#         --name $name \
-#         --model_name $teacher_model\
-#         --dataset $dataset\
-#         --cutout_length $cutout\
-#         --batch_size $batch_size \
-#         --epochs $epoch \
-#         --seed $seed \
-#         --save E$epoch \
-#         --train_portion $train_portion
-# done
-
+        name=$2
+        teacher_model=$3
+        save=$4
+        description=$5
+        dataset=cifar100
+        cutout=16
+        epoch=100
+        seed=0
+        train_portion=0.9
+        python trainTeacher_main.py \
+            --name $name \
+            --model_name $teacher_model\
+            --dataset $dataset\
+            --cutout_length $cutout\
+            --epochs $epoch \
+            --seed $seed \
+            --save $save \
+            --train_portion $train_portion \
+            --lr 0.1 \
+            --description $description \
+            --batch_size 16
+elif [ ${type} = "test" ]; then
+# ===== モデルをテスト =====
+        save=$2
+        teacher_model=$3
+        resume_path=$4
+        dataset=cifar100
+        cutout=0
+        batch_size=64
+        seed=0
+        train_portion=1.0
+        python testTeacher_main.py \
+                --save $save \
+                --model_name $teacher_model \
+                --resume_path $resume_path \
+                --dataset $dataset\
+                --seed $seed
+else
+    echo ""
+fi
 ## ===== 複数モデルをファインチューニングする =====
 # name=$1
 # dataset=cifar100
@@ -83,22 +65,6 @@
 #         --save E$epoch \
 #         --train_portion $train_portion
 # done
-
-## ===== モデルをテスト =====
-save=$1
-teacher_model=$2
-resume_path=$3
-dataset=cifar100
-cutout=0
-batch_size=64
-seed=0
-train_portion=1.0
-python testTeacher_main.py \
-        --save $save \
-        --model_name $teacher_model \
-        --resume_path $resume_path \
-        --dataset $dataset\
-        --seed $seed
 
 ## ===== 複数モデルをテスト =====
 # save=$1
@@ -125,3 +91,27 @@ python testTeacher_main.py \
 #         --dataset $dataset\
 #         --seed $seed
 # done
+
+# ===== モデルをファインチューニング ===== 廃止
+# name=$1
+
+# teacher_model=$2
+# save=$3
+# dataset=cifar100
+# cutout=16
+# batch_size=64
+# epoch=300
+# seed=0
+# train_portion=0.9
+# python finetuneTeacher_main.py \
+#     --name $name \
+#     --model_name $teacher_model\
+#     --dataset $dataset\
+#     --cutout_length $cutout\
+#     --batch_size $batch_size \
+#     --epochs $epoch \
+#     --seed $seed \
+#     --save $save \
+#     --train_portion $train_portion \
+#     --w_lr 0.1 \
+#     --w_grad_clip 100.
