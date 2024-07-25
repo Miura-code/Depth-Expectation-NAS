4f8ad1ad9e83e86a618e3a5a213f54aad5b327ee
miuraSynapse <smiuymiu0official0@gmail.com>
2024-07-25 20:43:52 +0900

    add: セルレベル探索でPartiallyConnectionができるように

--- models/search_cell.py
+++ models/search_cell.py
@@ -41,3 +41,35 @@ class SearchCell(nn.Module):
 
         s_out = torch.cat(states[2:], dim=1)
         return s_out
+    
+class SearchCellPartialConnection(nn.Module):
+    def __init__(self, n_nodes, C_pp, C_p, C, reduction_p, reduction):
+        super().__init__()
+        self.reduction = reduction
+        self.n_nodes = n_nodes
+
+        if reduction_p:
+            self.preproc0 = ops.FactorizedReduce(C_pp, C, affine=False)
+        else:
+            self.preproc0 = ops.StdConv(C_pp, C, 1, 1, 0, affine=False)
+        self.preproc1 = ops.StdConv(C_p, C, 1, 1, 0, affine=False)
+
+        self.dag = nn.ModuleList()
+        for i in range(self.n_nodes):
+            self.dag.append(nn.ModuleList())
+            for j in range(2 + i):
+                stride = 2 if reduction and j < 2 else 1
+                op = ops.MixedOpPC(C, stride)
+                self.dag[i].append(op)
+    
+    def forward(self, s0, s1, w_dag, bw_dag):
+        s0 = self.preproc0(s0)
+        s1 = self.preproc1(s1)
+
+        states = [s0, s1]
+        for edges, w_list, bw_list in zip(self.dag, w_dag, bw_dag):
+            s_cur = sum(b * edges[i](s, w) for i, (s, w, b) in enumerate(zip(states, w_list, bw_list)))
+            states.append(s_cur)
+
+        s_out = torch.cat(states[2:], dim=1)
+        return s_out
