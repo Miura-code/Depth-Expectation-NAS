b6dfdbb6beb5534690bd1b45b2e4f075fb5fef9a
miuraSynapse <smiuymiu0official0@gmail.com>
2024-07-26 10:50:36 +0900

    add: BASELINE224を追加定義

--- genotypes/genotype_cell.py
+++ genotypes/genotype_cell.py
@@ -222,3 +222,40 @@ BASELINE2 = Genotype3(
     ], 
     normal3_concat=range(2, 6)
 ) 
+
+BASELINE224 = Genotype3(
+    normal1=[
+        [('sep_conv_3x3', 0), ('sep_conv_3x3', 1)], 
+        [('sep_conv_3x3', 0), ('sep_conv_3x3', 1)], 
+        [('sep_conv_3x3', 1), ('dil_conv_3x3', 2)], 
+        [('sep_conv_3x3', 1), ('sep_conv_3x3', 3)]
+    ], 
+    normal1_concat=range(2, 6), 
+    reduce1=[
+        [('skip_connect', 1), ('sep_conv_5x5', 0)], 
+        [('sep_conv_3x3', 2), ('sep_conv_5x5', 0)], 
+        [('sep_conv_3x3', 0), ('sep_conv_3x3', 2)], 
+        [('max_pool_3x3', 0), ('max_pool_3x3', 1)]
+    ], 
+    reduce1_concat=range(2, 6), 
+    normal2=[
+        [('skip_connect', 0), ('skip_connect', 1)], 
+        [('skip_connect', 0), ('dil_conv_3x3', 2)], 
+        [('skip_connect', 0), ('skip_connect', 2)], 
+        [('skip_connect', 0), ('skip_connect', 1)]
+    ], 
+    normal2_concat=range(2, 6), 
+    reduce2=[
+        [('avg_pool_3x3', 0), ('avg_pool_3x3', 1)], 
+        [('avg_pool_3x3', 0), ('skip_connect', 2)], 
+        [('avg_pool_3x3', 0), ('skip_connect', 2)], 
+        [('skip_connect', 2), ('avg_pool_3x3', 0)]
+    ], reduce2_concat=range(2, 6), 
+    normal3=[
+        [('skip_connect', 0), ('skip_connect', 1)], 
+        [('skip_connect', 0), ('dil_conv_5x5', 1)], 
+        [('skip_connect', 0), ('dil_conv_3x3', 1)], 
+        [('skip_connect', 0), ('skip_connect', 1)]
+    ], 
+    normal3_concat=range(2, 6)
+)
