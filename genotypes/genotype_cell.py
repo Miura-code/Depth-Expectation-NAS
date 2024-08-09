9758c7425f238b808dcae8bd3d2cd9d797b207f3
miuraSynapse <smiuymiu0official0@gmail.com>
2024-08-09 19:03:26 +0900

    add: セルレベル構造のベースラインベスト構造を登録

--- genotypes/genotype_cell.py
+++ genotypes/genotype_cell.py
@@ -259,3 +259,41 @@ BASELINE224 = Genotype3(
     ], 
     normal3_concat=range(2, 6)
 )
+
+BASELINE_BEST = Genotype3(
+    normal1=[
+        [('sep_conv_5x5', 0), ('sep_conv_3x3', 1)], 
+        [('sep_conv_3x3', 1), ('sep_conv_3x3', 0)], 
+        [('sep_conv_3x3', 1), ('sep_conv_3x3', 2)], 
+        [('sep_conv_3x3', 0), ('sep_conv_5x5', 4)]
+    ], 
+    normal1_concat=range(2, 6), 
+    reduce1=[
+        [('sep_conv_3x3', 0), ('skip_connect', 1)], 
+        [('sep_conv_3x3', 1), ('max_pool_3x3', 0)], 
+        [('sep_conv_3x3', 2), ('sep_conv_3x3', 1)], 
+        [('max_pool_3x3', 0), ('dil_conv_3x3', 1)]
+    ], 
+    reduce1_concat=range(2, 6), 
+    normal2=[
+        [('skip_connect', 0), ('sep_conv_5x5', 1)], 
+        [('skip_connect', 0), ('skip_connect', 2)], 
+        [('avg_pool_3x3', 0), ('avg_pool_3x3', 2)], 
+        [('skip_connect', 0), ('avg_pool_3x3', 2)]
+    ], 
+    normal2_concat=range(2, 6), 
+    reduce2=[
+        [('avg_pool_3x3', 0), ('avg_pool_3x3', 1)], 
+        [('avg_pool_3x3', 0), ('skip_connect', 2)], 
+        [('skip_connect', 2), ('avg_pool_3x3', 0)], 
+        [('skip_connect', 2), ('avg_pool_3x3', 0)]
+    ], 
+    reduce2_concat=range(2, 6), 
+    normal3=[
+        [('skip_connect', 0), ('skip_connect', 1)], 
+        [('skip_connect', 0), ('dil_conv_3x3', 1)], 
+        [('skip_connect', 0), ('skip_connect', 1)], 
+        [('skip_connect', 0), ('skip_connect', 2)]
+    ], 
+    normal3_concat=range(2, 6)
+)
