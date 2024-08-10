8efed621378b2bbe8ec1321fcf672ebeb85d0b87
miura-Miura <smiuymiu0official0@gmail.com>
2024-08-10 17:55:53 +0900

    experiment:

--- run_bash.sh
+++ run_bash.sh
@@ -5,7 +5,7 @@ teacher_path=/home/miura/lab/KD-hdas/results/teacher/cifar100/efficientnet_v2_s/
 # bash run_finetune.sh train FINETUNE2 efficientnet_v2_m pretrained pretrained_LR_features-0.001_classifier-0.01_cosine_warmup-0
 
 for seed in 0 1 2 3 4;do
-    bash run_search.sh train stage BASELINE224 efficientnet_v2_s $teacher_path s${seed} BASELINE_BEST KD_for_stage_architecture_search_kd_for_architecture_parameters ${seed}
+    bash run_search.sh train stage ONLY_ARCH efficientnet_v2_s $teacher_path s${seed} BASELINE_BEST KD_for_stage_architecture_search_kd_for_architecture_parameters ${seed}
 done
 
 # Ls=(0.3 0.4 0.5 0.6 0.7)
