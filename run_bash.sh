01eb0d72cb4155e4101da92fcf66d5351d5d5b09
miuraSynapse <smiuymiu0official0@gmail.com>
2024-12-07 18:27:56 +0900

    experiment: カリキュラム探索を追加

--- run_bash.sh
+++ run_bash.sh
@@ -2,74 +2,73 @@
 teacher_path=/home/miura/lab/KD-hdas/results/evaluate_stage_KD/cifar100/noDepthLoss/s0-BaselineBestCell/best.pth.tar
 genotype=BASELINE_BEST
 
-experiment_name=Pruning
+experiment_name=Curriculum
 
-## 構造のMACを計算する
-genotype=$1
-DAG=$2
-channels=16
-layers=20
-python cal_macs_main.py \
-    --genotype $genotype\
-    --DAG $DAG\
-    --spec_cell \
-    --init_channels $channels\
-    --layers $layers\
-    --aux_weight 0.0\
-    --drop_path_prob 0.0\
-    --dataset cifar100\
-    --advanced
+# ## 構造のMACを計算する
+# genotype=$1
+# DAG=$2
+# channels=16
+# layers=20
+# python cal_macs_main.py \
+#     --genotype $genotype\
+#     --DAG $DAG\
+#     --spec_cell \
+#     --init_channels $channels\
+#     --layers $layers\
+#     --aux_weight 0.0\
+#     --drop_path_prob 0.0\
+#     --dataset cifar100\
+#     --advanced
     
 
-# for seed in 0;do
-#     for g in 0.001 0.01 0.1 1 -0.001 -0.01 -0.1 -1;do
-#     # for g in 0.001;do
-#         for method in expected;do
-#             bash run_searchStage.sh train Pruning \
-#             $experiment_name none none s$seed-${method}2-sw3-g$g\
-#             $genotype \
-#             search_and_evaluation_curriculum_learning_with_-Beta-expected-Depth-ecpected-constraint_slidewindow-3 \
-#             $seed 0 0 $g\
-#             1 0 3 1 0\
-#             $method "30 20"
+for seed in 0;do
+    for g in -0.001 -0.01 -0.1 -1;do
+        for method in expected;do
+            bash run_searchStage.sh train SearchEvalCurriculum \
+            $experiment_name none none s$seed-${method}2-sw3-g${g}_30-20\
+            $genotype \
+            search_and_evaluation_curriculum_learning_with_-Beta-expected-Depth-ecpected-constraint_slidewindow-3 \
+            $seed 0 0 $g\
+            1 0 3 1 0\
+            $method "30 20"
 
-# #             # dir=/home/miura/lab/KD-hdas/results/search_stage_KD/cifar100/$experiment_name/s$seed-$method-sw3-g$g/DAG
-# #             # dag=$(find "$dir" -type f -name '*best*' -exec stat --format="%Y %n" {} + | sort -nr | head -n 1 | awk '{print $2}')
-# #             # path=/home/miura/lab/KD-hdas/results/search_stage_KD/cifar100/$experiment_name/s$seed-$method-sw3-g$g/best.pth.tar
+#             # dir=/home/miura/lab/KD-hdas/results/search_stage_KD/cifar100/$experiment_name/s$seed-$method-sw3-g$g/DAG
+#             # dag=$(find "$dir" -type f -name '*best*' -exec stat --format="%Y %n" {} + | sort -nr | head -n 1 | awk '{print $2}')
+#             # path=/home/miura/lab/KD-hdas/results/search_stage_KD/cifar100/$experiment_name/s$seed-$method-sw3-g$g/best.pth.tar
 
-# #             # bash run_evaluate.sh train stage Pruning \
-# #             # none none \
-# #             # $genotype $dag \
-# #             # s$seed-$method-sw3-g$g \
-# #             # search_with_L1-Alpha-constraint_slidewindow-3 \
-# #             # 0
+#             # bash run_evaluate.sh train stage Pruning \
+#             # none none \
+#             # $genotype $dag \
+#             # s$seed-$method-sw3-g$g \
+#             # search_with_L1-Alpha-constraint_slidewindow-3 \
+#             # 0
 
-# #             # path=/home/miura/lab/KD-hdas/results/evaluate_stage_KD/cifar100/$experiment_name/s$seed-$method-sw3-g$g/best.pth.tar
-# #             # bash run_evaluate.sh test stage $genotype $dag $path
-#         done
-# #         # bash run_searchStage.sh train ArchKD \
-# #         # $experiment_name h_das_224baseline $teacher_path s$seed-alphal1-sw3-g$g\
-# #         # $genotype \
-# #         # search_with_L1-Alpha-constraint_slidewindow-3 \
-# #         # $seed $g $g $g\
-# #         # 1 0 3 0 0\
-# #         # alphal1
+#             # path=/home/miura/lab/KD-hdas/results/evaluate_stage_KD/cifar100/$experiment_name/s$seed-$method-sw3-g$g/best.pth.tar
+#             # bash run_evaluate.sh test stage $genotype $dag $path
+        done
+#         # bash run_searchStage.sh train ArchKD \
+#         # $experiment_name h_das_224baseline $teacher_path s$seed-alphal1-sw3-g$g\
+#         # $genotype \
+#         # search_with_L1-Alpha-constraint_slidewindow-3 \
+#         # $seed $g $g $g\
+#         # 1 0 3 0 0\
+#         # alphal1
 
-# #         # dir=/home/miura/lab/KD-hdas/results/search_stage_KD/cifar100/$experiment_name/s$seed-alphal1-sw3-g$g/DAG
-# #         # dag=$(find "$dir" -type f -name '*best*' -exec stat --format="%Y %n" {} + | sort -nr | head -n 1 | awk '{print $2}')
-# #         # path=/home/miura/lab/KD-hdas/results/search_stage_KD/cifar100/$experiment_name/s$seed-alphal1-sw3-g$g/best.pth.tar
+#         # dir=/home/miura/lab/KD-hdas/results/search_stage_KD/cifar100/$experiment_name/s$seed-alphal1-sw3-g$g/DAG
+#         # dag=$(find "$dir" -type f -name '*best*' -exec stat --format="%Y %n" {} + | sort -nr | head -n 1 | awk '{print $2}')
+#         # path=/home/miura/lab/KD-hdas/results/search_stage_KD/cifar100/$experiment_name/s$seed-alphal1-sw3-g$g/best.pth.tar
 
-# #         # bash run_evaluate.sh train stage ArchKD \
-# #         # none none \
-# #         # $genotype $dag \
-# #         # s$seed-alphal1-sw3-g$g \
-# #         # search_with_L1-Alpha-constraint_slidewindow-3 \
-# #         # 0
+#         # bash run_evaluate.sh train stage ArchKD \
+#         # none none \
+#         # $genotype $dag \
+#         # s$seed-alphal1-sw3-g$g \
+#         # search_with_L1-Alpha-constraint_slidewindow-3 \
+#         # 0
 
-# #         # path=/home/miura/lab/KD-hdas/results/evaluate_stage_KD/cifar100/$experiment_name/s$seed-alphal1-sw3-g$g/best.pth.tar
-# #         # bash run_evaluate.sh test stage $genotype $dag $path
-#     done
-# done
+#         # path=/home/miura/lab/KD-hdas/results/evaluate_stage_KD/cifar100/$experiment_name/s$seed-alphal1-sw3-g$g/best.pth.tar
+#         # bash run_evaluate.sh test stage $genotype $dag $path
+    done
+done
 
 # dir=/home/miura/lab/KD-hdas/results/search_stage_KD/cifar100/$experiment_name/s0-BaselineBestCell/DAG
 # dag=$(find "$dir" -type f -name '*best*' -exec stat --format="%Y %n" {} + | sort -nr | head -n 1 | awk '{print $2}')
