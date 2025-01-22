b58d7bb9b98fe71a3eed6aa89df19d3f417820f7
miura-Miura <smiuymiu0official0@gmail.com>
2025-01-22 22:34:09 +0900

    doc: 論文に関係あるファイルのみ整理

--- teacher_models/hint_layer_settings.py
+++
@@ -1,6 +0,0 @@
-efficientnet_v2_s_HINTLAYERS = {
-    'features.3.3.block.1': 'stage1',
-    'features.4.5.block.3': 'stage2',
-    'features.6.14.block.3': 'stage3',
-    'classifier.1': 'logits'
-}
