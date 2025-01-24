c3c37d3f68df4063d8669eb9189730c3b78cda76
miura-Miura <smiuymiu0official0@gmail.com>
2025-01-24 17:57:22 +0900

    doc: 不要な実験を削除

--- run_finetune2.sh
+++
@@ -1,5 +0,0 @@
-target_directory="/home/miura/lab/KD-hdas/results/search_stage_KD/cifar100/noDepthLoss/s0-BaselineBestCell/DAG"
-
-# ディレクトリ内で「best」を含むファイルをリストアップし、最も新しいファイルを見つける
-newest_file=$(find "$target_directory" -type f -name '*best*' -exec stat --format="%Y %n" {} + | sort -nr | head -n 1 | awk '{print $2}')
-echo $newest_file
